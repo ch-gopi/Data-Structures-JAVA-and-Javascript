f17101d3a7ffcfcd0963b339cb9ff90b4c936831
GOPI CHOWDAM <ch.gopi7893@gmail.com>
2023-11-04 01:46:12 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (7.90%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,15 +15,12 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
-        
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    public boolean isValidBST(TreeNode root, long min, long max){
-        if (root==null) 
-          return true;
-        if (root.val>=max || root.val <=min)
-         return false;
-        return isValidBST(root.left,min,root.val) && isValidBST(root.right,root.val,max) ;
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
-    
-};
+}
